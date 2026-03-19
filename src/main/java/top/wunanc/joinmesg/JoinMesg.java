78e7dad16842256cb0e705cd5d81c4d1c5ec9103
wunanc <wunanc@163.com>
2026-03-19 16:25:22 +0800

    first commit

---
+++ src/main/java/top/wunanc/joinmesg/JoinMesg.java
@@ -0,0 +1,16 @@
+package top.wunanc.joinmesg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JoinMesg extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
